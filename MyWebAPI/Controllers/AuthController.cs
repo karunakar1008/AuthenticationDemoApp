7dc94354e91de558552bc96cfcfda5f1195d916f
karunakarbhogyari <karunakar1008@gmail.com>
2022-05-21 12:32:40 +0530

    Secure web api using jwt token.

---
+++ MyWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] Credential credential)
+        {
+
+            //Verify credential
+            if (credential.UserName == "admin" && credential.Password == "password")
+            {
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, "admin")
+                    , new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                    , new Claim("Department", "HR")
+                    , new Claim("Admin", "true")
+                    , new Claim("Manager", "true")
+                    ,new Claim("EmploymentDate","2022-02-18")
+                };
+
+                var expiresAt = DateTime.UtcNow.AddMinutes(10);
+                return Ok(
+                    new
+                    {
+                        access_token = CreateToken(claims, expiresAt),
+                        expires_at = expiresAt
+                    });
+            }
+            ModelState.AddModelError("UnAuthorized", "You are not authorized to access endpoint");
+            return Unauthorized(ModelState);
+        }
+
+        [NonAction]
+        public string CreateToken(IEnumerable<Claim> claims, DateTime expiresAt)
+        {
+            var secretKey = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("SecretKey"));
+
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expiresAt,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature));
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+    public class Credential
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
